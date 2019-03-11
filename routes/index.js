4ebcd8445967b8cb7c4997d12ad8e00c78800ad5
kikeanaya <luiseanayam@gmail.com>
2019-03-11 18:59:52 +0100

    base for monday after class

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const router  = express.Router()
+const axios = require('axios')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index')
 })
 
-module.exports = router;
+// const getRecipeInfo = (arg) => {
+//     axios
+//     .get(`https://api.edamam.com/search?q=chicken&app_id=APIID&app_key=APIKEY&from=0&to=1&diet=high-protein`)
+//     .then(res => {
+//         console.log(res.data.hits)
+//     })
+//     .catch(err => console.log("An error ocurred: ", err))
+// }
+
+// getRecipeInfo()
+
+module.exports = router
